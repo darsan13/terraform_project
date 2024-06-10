41ec20ce0f9db26a9177f5710e2323dd98343890
DevOpsHub <help.devops@gmail.com>
2024-06-10 06:43:32 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
+  type        = string
+}
+
+variable "ecr_repo_name" {
+  description = "The name of the ECR repository"
+  type        = string
+  default     = "nginx-repo"
+}
