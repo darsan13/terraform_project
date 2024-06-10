8c83023eb45e3964e4616ef8c1af2b6f7340688c
DevOpsHub <help.devops@gmail.com>
2024-06-10 06:44:47 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.web.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.main.repository_url
+}
